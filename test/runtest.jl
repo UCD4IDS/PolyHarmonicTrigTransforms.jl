f24136ab78a96b87ce854f77adeade4d2e84b57a
Jason Y <4108945+jason-yoon21@users.noreply.github.com>
2025-01-07 20:22:29 -0800

    test file

--- test/runtest.jl
+++ test/runtest.jl
@@ -4,21 +4,21 @@ using .PolyHarmonicTrigTransforms
 
 #check for dst
 has_dst = isdefined(PolyHarmonicTrigTransforms, :dst);
-@assert has_dst = true;
+@test has_dst = true;
 
 has_idst = isdefined(PolyHarmonicTrigTransforms, :idst);
-@assert has_idst = true;
+@test has_idst = true;
 
 has_phlct_backward = isdefined(PolyHarmonicTrigTransforms, :phlct_backward);
-@assert has_phlct_backward = true;
+@test has_phlct_backward = true;
 
 has_phlct_forward = isdefined(PolyHarmonicTrigTransforms, :phlct_forward);
-@assert has_phlct_forward = true;
+@test has_phlct_forward = true;
 
 has_phlct_restore = isdefined(PolyHarmonicTrigTransforms, :phlct_restore);
-@assert has_phlct_restore = true;
+@test has_phlct_restore = true;
 
 has_phlct2d = isdefined(PolyHarmonicTrigTransforms, :phlct2d);
-@assert has_phlct2d = true;
+@test has_phlct2d = true;
 
-print("assertions passed");
+print("tests passed");
